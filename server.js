70c60477403185ca839ca8805b23704ae3528d5b
Oluwaseyi Olufon <seyi.olufon@gmail.com>
2025-07-17 10:04:36 -0600

    step4 setting docker and pushing image

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-let userGoal = 'Learn Docker!';
+let userGoal = 'Learn and Testing Pipeline!';
 
 app.use(
   bodyParser.urlencoded({
